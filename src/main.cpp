07e6475d40c1b3290795e818a752509a9231d70a
Mat <tornado@amu.edu.pl>
2021-02-19 12:18:11 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1778,6 +1778,7 @@ public:
   double MUHeight();
   double MinTHTEHeight();
   double DeltaThetaE();
+  double DeltaThetaE_HGL();
   double VDCAPE();	
   double VirtualColdPoolStrength();
   double WindIndex();
@@ -1812,6 +1813,12 @@ public:
   double SRH500LM();
   double SRH01LM();
   double SRH03LM();
+  double Bunkers_RM_A();
+  double Bunkers_RM_M();
+  double Bunkers_LM_A();
+  double Bunkers_LM_M();
+  double Bunkers_MW_A();
+  double Bunkers_MW_M();
   double K_Index();
   double Showalter_Index();	
   double TotalTotals();		
@@ -1828,7 +1835,6 @@ public:
   double MU_EFF_WMAXSHEAR();
   double SB_EFF_WMAXSHEAR();
   double ML_EFF_WMAXSHEAR();
-  double NEWDTE();
 };
 
 //--------------------------------------------------------------------------------------------------------------------
@@ -3042,11 +3048,54 @@ double IndicesCollector::RHMIDDLE(){
 	return S->th->meanhumMIDDLE;
 }
 
+double IndicesCollector::Bunkers_RM_A(){
+	double *tab = S->ks->rm->toAV(); 
+	double angle = tab[0];
+	delete[] tab;
+	return angle;
+}
+
+double IndicesCollector::Bunkers_RM_M(){
+	double *tab = S->ks->rm->toAV(); 
+	double magnitude = tab[1]; 
+	delete[] tab;
+	return magnitude;
+}
+
+double IndicesCollector::Bunkers_LM_A(){
+	double *tab = S->ks->lm->toAV(); 
+	double angle = tab[0];
+	delete[] tab;
+	return angle;
+}
+
+double IndicesCollector::Bunkers_LM_M(){
+	double *tab = S->ks->lm->toAV(); 
+	double magnitude = tab[1]; 
+	delete[] tab;
+	return magnitude;
+}
+
+double IndicesCollector::Bunkers_MW_A(){
+	double *tab = this->mean06->toAV(); 
+	double angle = tab[0];
+	delete[] tab;
+	return angle;
+}
+
+double IndicesCollector::Bunkers_MW_M(){
+	double *tab = this->mean06->toAV(); 
+	double magnitude = tab[1]; 
+	delete[] tab;
+	return magnitude;
+}
+
+
 //##############################################################################################
 
 double * processSounding(double *p_, double *h_, double *t_, double *d_, double *a_, double *v_, int length, double dz, Sounding **S){
   *S = new Sounding(p_,h_,t_,d_,a_,v_,length, dz);
-  double * vec = new double[101];
+  double * vec = new double[107];
   vec[0]=(*S)->getIndicesCollectorPointer()->VMostUnstableCAPE();
   vec[1]=(*S)->getIndicesCollectorPointer()->VLLMostUnstableCAPE();
   vec[2]=(*S)->getIndicesCollectorPointer()->MUmiddlecape();
@@ -3097,57 +3146,63 @@ double * processSounding(double *p_, double *h_, double *t_, double *d_, double 
   vec[47]=(*S)->getIndicesCollectorPointer()->MUHeight();
   vec[48]=(*S)->getIndicesCollectorPointer()->MinTHTEHeight();
   vec[49]=(*S)->getIndicesCollectorPointer()->DeltaThetaE();
-  vec[50]=(*S)->getIndicesCollectorPointer()->VDCAPE(); 
-  vec[51]=(*S)->getIndicesCollectorPointer()->VirtualColdPoolStrength();
-  vec[52]=(*S)->getIndicesCollectorPointer()->WindIndex();
-  vec[53]=(*S)->getIndicesCollectorPointer()->PWATER();
-  vec[54]=(*S)->getIndicesCollectorPointer()->MoistureFlux();
-  vec[55]=(*S)->getIndicesCollectorPointer()->RH02();
-  vec[56]=(*S)->getIndicesCollectorPointer()->RH25();
-  vec[57]=(*S)->getIndicesCollectorPointer()->RHMIDDLE();  
-  vec[58]=(*S)->getIndicesCollectorPointer()->BS01();
-  vec[59]=(*S)->getIndicesCollectorPointer()->BS02();
-  vec[60]=(*S)->getIndicesCollectorPointer()->BS03();
-  vec[61]=(*S)->getIndicesCollectorPointer()->BS06();
-  vec[62]=(*S)->getIndicesCollectorPointer()->BS08();
-  vec[63]=(*S)->getIndicesCollectorPointer()->BS36();
-  vec[64]=(*S)->getIndicesCollectorPointer()->BS18();
-  vec[65]=(*S)->getIndicesCollectorPointer()->emubs();
-  vec[66]=(*S)->getIndicesCollectorPointer()->esbbs();
-  vec[67]=(*S)->getIndicesCollectorPointer()->emlbs();
-  vec[68]=(*S)->getIndicesCollectorPointer()->BulkShearSfcTen();
-  vec[69]=(*S)->getIndicesCollectorPointer()->BulkShearMULFCTen();
-  vec[70]=(*S)->getIndicesCollectorPointer()->BulkShearSBLFCTen();
-  vec[71]=(*S)->getIndicesCollectorPointer()->BulkShearMLLFCTen();
-  vec[72]=(*S)->getIndicesCollectorPointer()->MeanWind01();
-  vec[73]=(*S)->getIndicesCollectorPointer()->MeanWind02();
-  vec[74]=(*S)->getIndicesCollectorPointer()->MeanWind06();
-  vec[75]=(*S)->getIndicesCollectorPointer()->MeanWind13();
-  vec[76]=(*S)->getIndicesCollectorPointer()->SRH100RM();
-  vec[77]=(*S)->getIndicesCollectorPointer()->SRH500RM();
-  vec[78]=(*S)->getIndicesCollectorPointer()->SRH01RM();
-  vec[79]=(*S)->getIndicesCollectorPointer()->SRH03RM();
-  vec[80]=(*S)->getIndicesCollectorPointer()->SRH100LM();
-  vec[81]=(*S)->getIndicesCollectorPointer()->SRH500LM();
-  vec[82]=(*S)->getIndicesCollectorPointer()->SRH01LM();
-  vec[83]=(*S)->getIndicesCollectorPointer()->SRH03LM();
-  vec[84]=(*S)->getIndicesCollectorPointer()->K_Index();
-  vec[85]=(*S)->getIndicesCollectorPointer()->Showalter_Index(); 
-  vec[86]=(*S)->getIndicesCollectorPointer()->TotalTotals();  
-  vec[87]=(*S)->getIndicesCollectorPointer()->SWEATIndex(); 
-  vec[88]=(*S)->getIndicesCollectorPointer()->STP();
-  vec[89]=(*S)->getIndicesCollectorPointer()->STPeff();
-  vec[90]=(*S)->getIndicesCollectorPointer()->SCP();
-  vec[91]=(*S)->getIndicesCollectorPointer()->SCPeff();
-  vec[92]=(*S)->getIndicesCollectorPointer()->SHP();
-  vec[93]=(*S)->getIndicesCollectorPointer()->DCP();
-  vec[94]=(*S)->getIndicesCollectorPointer()->MU_WMAXSHEAR();
-  vec[95]=(*S)->getIndicesCollectorPointer()->SB_WMAXSHEAR();
-  vec[96]=(*S)->getIndicesCollectorPointer()->ML_WMAXSHEAR();
-  vec[97]=(*S)->getIndicesCollectorPointer()->MU_EFF_WMAXSHEAR();
-  vec[98]=(*S)->getIndicesCollectorPointer()->SB_EFF_WMAXSHEAR();
-  vec[99]=(*S)->getIndicesCollectorPointer()->ML_EFF_WMAXSHEAR();
-  vec[100]=(*S)->getIndicesCollectorPointer()->NEWDTE();
+  vec[50]=(*S)->getIndicesCollectorPointer()->DeltaThetaE_HGL();
+  vec[51]=(*S)->getIndicesCollectorPointer()->VDCAPE(); 
+  vec[52]=(*S)->getIndicesCollectorPointer()->VirtualColdPoolStrength();
+  vec[53]=(*S)->getIndicesCollectorPointer()->WindIndex();
+  vec[54]=(*S)->getIndicesCollectorPointer()->PWATER();
+  vec[55]=(*S)->getIndicesCollectorPointer()->MoistureFlux();
+  vec[56]=(*S)->getIndicesCollectorPointer()->RH02();
+  vec[57]=(*S)->getIndicesCollectorPointer()->RH25();
+  vec[58]=(*S)->getIndicesCollectorPointer()->RHMIDDLE();  
+  vec[59]=(*S)->getIndicesCollectorPointer()->BS01();
+  vec[60]=(*S)->getIndicesCollectorPointer()->BS02();
+  vec[61]=(*S)->getIndicesCollectorPointer()->BS03();
+  vec[62]=(*S)->getIndicesCollectorPointer()->BS06();
+  vec[63]=(*S)->getIndicesCollectorPointer()->BS08();
+  vec[64]=(*S)->getIndicesCollectorPointer()->BS36();
+  vec[65]=(*S)->getIndicesCollectorPointer()->BS18();
+  vec[66]=(*S)->getIndicesCollectorPointer()->emubs();
+  vec[67]=(*S)->getIndicesCollectorPointer()->esbbs();
+  vec[68]=(*S)->getIndicesCollectorPointer()->emlbs();
+  vec[69]=(*S)->getIndicesCollectorPointer()->BulkShearSfcTen();
+  vec[70]=(*S)->getIndicesCollectorPointer()->BulkShearMULFCTen();
+  vec[71]=(*S)->getIndicesCollectorPointer()->BulkShearSBLFCTen();
+  vec[72]=(*S)->getIndicesCollectorPointer()->BulkShearMLLFCTen();
+  vec[73]=(*S)->getIndicesCollectorPointer()->MeanWind01();
+  vec[74]=(*S)->getIndicesCollectorPointer()->MeanWind02();
+  vec[75]=(*S)->getIndicesCollectorPointer()->MeanWind06();
+  vec[76]=(*S)->getIndicesCollectorPointer()->MeanWind13();
+  vec[77]=(*S)->getIndicesCollectorPointer()->SRH100RM();
+  vec[78]=(*S)->getIndicesCollectorPointer()->SRH500RM();
+  vec[79]=(*S)->getIndicesCollectorPointer()->SRH01RM();
+  vec[80]=(*S)->getIndicesCollectorPointer()->SRH03RM();
+  vec[81]=(*S)->getIndicesCollectorPointer()->SRH100LM();
+  vec[82]=(*S)->getIndicesCollectorPointer()->SRH500LM();
+  vec[83]=(*S)->getIndicesCollectorPointer()->SRH01LM();
+  vec[84]=(*S)->getIndicesCollectorPointer()->SRH03LM();
+  vec[85]=(*S)->getIndicesCollectorPointer()->Bunkers_RM_A();
+  vec[86]=(*S)->getIndicesCollectorPointer()->Bunkers_RM_M();
+  vec[87]=(*S)->getIndicesCollectorPointer()->Bunkers_LM_A();
+  vec[88]=(*S)->getIndicesCollectorPointer()->Bunkers_LM_M();
+  vec[89]=(*S)->getIndicesCollectorPointer()->Bunkers_MW_A();
+  vec[90]=(*S)->getIndicesCollectorPointer()->Bunkers_MW_M();
+  vec[91]=(*S)->getIndicesCollectorPointer()->K_Index();
+  vec[92]=(*S)->getIndicesCollectorPointer()->Showalter_Index(); 
+  vec[93]=(*S)->getIndicesCollectorPointer()->TotalTotals();  
+  vec[94]=(*S)->getIndicesCollectorPointer()->SWEATIndex(); 
+  vec[95]=(*S)->getIndicesCollectorPointer()->STP();
+  vec[96]=(*S)->getIndicesCollectorPointer()->STPeff();
+  vec[97]=(*S)->getIndicesCollectorPointer()->SCP();
+  vec[98]=(*S)->getIndicesCollectorPointer()->SCPeff();
+  vec[99]=(*S)->getIndicesCollectorPointer()->SHP();
+  vec[100]=(*S)->getIndicesCollectorPointer()->DCP();
+  vec[101]=(*S)->getIndicesCollectorPointer()->MU_WMAXSHEAR();
+  vec[102]=(*S)->getIndicesCollectorPointer()->SB_WMAXSHEAR();
+  vec[103]=(*S)->getIndicesCollectorPointer()->ML_WMAXSHEAR();
+  vec[104]=(*S)->getIndicesCollectorPointer()->MU_EFF_WMAXSHEAR();
+  vec[105]=(*S)->getIndicesCollectorPointer()->SB_EFF_WMAXSHEAR();
+  vec[106]=(*S)->getIndicesCollectorPointer()->ML_EFF_WMAXSHEAR();
   return vec;
 }
 
@@ -3172,7 +3227,7 @@ void listToArray(list<double> list, double * arr, int len){
 
 int interpolate(double **pu, double **hu, double **tu, double **du, double **au, double **vu, int n, double *p_arr=0, double *h_arr=0, int m=0, int o=0){
     double defp[]={850,700,500}; int plen = 3;
-    double defh[]={0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680, 690, 700, 710, 720, 730, 740, 750, 775, 800, 825, 850, 875, 900, 925, 950, 975, 1000, 1025, 1050, 1075, 1100, 1125, 1150, 1175, 1200, 1225, 1250, 1275, 1300, 1325, 1350, 1375, 1400, 1425, 1450, 1475, 1500, 1525, 1550, 1575, 1600, 1625, 1650, 1675, 1700, 1725, 1750, 1775, 1800, 1825, 1850, 1875, 1900, 1925, 1950, 1975, 2000, 2025, 2050, 2075, 2100, 2125, 2150, 2175, 2200, 2225, 2250, 2275, 2300, 2325, 2350, 2375, 2400, 2425, 2450, 2475, 2500, 2525, 2550, 2575, 2600, 2625, 2650, 2675, 2700, 2725, 2750, 2775, 2800, 2825, 2850, 2875, 2900, 2925, 2950, 2975, 3000, 3050, 3100, 3150, 3200, 3250, 3300, 3350, 3400, 3450, 3500, 3550, 3600, 3650, 3700, 3750, 3800, 3850, 3900, 3950, 4000, 4050, 4100, 4150, 4200, 4250, 4300, 4350, 4400, 4450, 4500, 4550, 4600, 4650, 4700, 4750, 4800, 4850, 4900, 4950, 5000, 5050, 5100, 5150, 5200, 5250, 5300, 5350, 5400, 5450, 5500, 5550, 5600, 5650, 5700, 5750, 5800, 5850, 5900, 5950, 6000, 6100, 6200, 6300, 6400, 6500, 6600, 6700, 6800, 6900, 7000, 7100, 7200, 7300, 7400, 7500, 7600, 7700, 7800, 7900, 8000, 8100, 8200, 8300, 8400, 8500, 8600, 8700, 8800, 8900, 9000, 9100, 9200, 9300, 9400, 9500, 9600, 9700, 9800, 9900, 10000, 10100, 10200, 10300, 10400, 10500, 10600, 10700, 10800, 10900, 11000, 11100, 11200, 11300, 11400, 11500, 11600, 11700, 11800, 11900, 12000, 12250, 12500, 12750, 13000, 13250, 13500, 13750, 14000, 14250, 14500, 14750, 15000, 15250, 15500, 15750, 16000, 16250, 16500, 16750, 17000, 17250, 17500, 17750, 18000, 18250, 18500, 18750, 19000, 19250, 19500, 19750, 20000}; int hlen = 318;
+    double defh[]={0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000, 3250, 3500, 3750, 4000, 4250, 4500, 4750, 5000, 5250, 5500, 5750, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000, 11500, 12000, 12500, 13000, 13500, 14000, 15000, 16000, 17000, 18000, 19000, 20000}; int hlen = 57;
     
     double * p = *pu;
     double * h = *hu;
@@ -3284,7 +3339,7 @@ int interpolate(double **pu, double **hu, double **tu, double **du, double **au,
 }
 int interpolate2(double **pu, double **hu, double **tu, double **du, double **au, double **vu, int n, double *p_arr=0, double *h_arr=0, int m=0, int o=0){
     double defp[]={850,700,500}; int plen = 3;
-    double defh[]={0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680, 690, 700, 710, 720, 730, 740, 750, 775, 800, 825, 850, 875, 900, 925, 950, 975, 1000, 1025, 1050, 1075, 1100, 1125, 1150, 1175, 1200, 1225, 1250, 1275, 1300, 1325, 1350, 1375, 1400, 1425, 1450, 1475, 1500, 1525, 1550, 1575, 1600, 1625, 1650, 1675, 1700, 1725, 1750, 1775, 1800, 1825, 1850, 1875, 1900, 1925, 1950, 1975, 2000, 2025, 2050, 2075, 2100, 2125, 2150, 2175, 2200, 2225, 2250, 2275, 2300, 2325, 2350, 2375, 2400, 2425, 2450, 2475, 2500, 2525, 2550, 2575, 2600, 2625, 2650, 2675, 2700, 2725, 2750, 2775, 2800, 2825, 2850, 2875, 2900, 2925, 2950, 2975, 3000, 3050, 3100, 3150, 3200, 3250, 3300, 3350, 3400, 3450, 3500, 3550, 3600, 3650, 3700, 3750, 3800, 3850, 3900, 3950, 4000, 4050, 4100, 4150, 4200, 4250, 4300, 4350, 4400, 4450, 4500, 4550, 4600, 4650, 4700, 4750, 4800, 4850, 4900, 4950, 5000, 5050, 5100, 5150, 5200, 5250, 5300, 5350, 5400, 5450, 5500, 5550, 5600, 5650, 5700, 5750, 5800, 5850, 5900, 5950, 6000, 6100, 6200, 6300, 6400, 6500, 6600, 6700, 6800, 6900, 7000, 7100, 7200, 7300, 7400, 7500, 7600, 7700, 7800, 7900, 8000, 8100, 8200, 8300, 8400, 8500, 8600, 8700, 8800, 8900, 9000, 9100, 9200, 9300, 9400, 9500, 9600, 9700, 9800, 9900, 10000, 10100, 10200, 10300, 10400, 10500, 10600, 10700, 10800, 10900, 11000, 11100, 11200, 11300, 11400, 11500, 11600, 11700, 11800, 11900, 12000, 12250, 12500, 12750, 13000, 13250, 13500, 13750, 14000, 14250, 14500, 14750, 15000, 15250, 15500, 15750, 16000, 16250, 16500, 16750, 17000, 17250, 17500, 17750, 18000, 18250, 18500, 18750, 19000, 19250, 19500, 19750, 20000}; int hlen = 318;
+    double defh[]={0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000, 3250, 3500, 3750, 4000, 4250, 4500, 4750, 5000, 5250, 5500, 5750, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000, 11500, 12000, 12500, 13000, 13500, 14000, 15000, 16000, 17000, 18000, 19000, 20000}; int hlen = 57;
   
     double * p = *pu;
     double * h = *hu;
@@ -3418,7 +3473,8 @@ double * sounding_default2(double* pressure,
     a[i]=angle[i];
     v[i]=velocity[i];
   }
-  double defh2[]={0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000, 3250, 3500, 3750, 4000, 4250, 4500, 4750, 5000, 5250, 5500, 5750, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000, 11500, 12000, 12500, 13000, 13500, 14000, 15000, 16000, 17000, 18000, 19000, 20000}; int hlen = 57;
+	
+  double defh2[]={0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680, 690, 700, 710, 720, 730, 740, 750, 775, 800, 825, 850, 875, 900, 925, 950, 975, 1000, 1025, 1050, 1075, 1100, 1125, 1150, 1175, 1200, 1225, 1250, 1275, 1300, 1325, 1350, 1375, 1400, 1425, 1450, 1475, 1500, 1525, 1550, 1575, 1600, 1625, 1650, 1675, 1700, 1725, 1750, 1775, 1800, 1825, 1850, 1875, 1900, 1925, 1950, 1975, 2000, 2025, 2050, 2075, 2100, 2125, 2150, 2175, 2200, 2225, 2250, 2275, 2300, 2325, 2350, 2375, 2400, 2425, 2450, 2475, 2500, 2525, 2550, 2575, 2600, 2625, 2650, 2675, 2700, 2725, 2750, 2775, 2800, 2825, 2850, 2875, 2900, 2925, 2950, 2975, 3000, 3050, 3100, 3150, 3200, 3250, 3300, 3350, 3400, 3450, 3500, 3550, 3600, 3650, 3700, 3750, 3800, 3850, 3900, 3950, 4000, 4050, 4100, 4150, 4200, 4250, 4300, 4350, 4400, 4450, 4500, 4550, 4600, 4650, 4700, 4750, 4800, 4850, 4900, 4950, 5000, 5050, 5100, 5150, 5200, 5250, 5300, 5350, 5400, 5450, 5500, 5550, 5600, 5650, 5700, 5750, 5800, 5850, 5900, 5950, 6000, 6100, 6200, 6300, 6400, 6500, 6600, 6700, 6800, 6900, 7000, 7100, 7200, 7300, 7400, 7500, 7600, 7700, 7800, 7900, 8000, 8100, 8200, 8300, 8400, 8500, 8600, 8700, 8800, 8900, 9000, 9100, 9200, 9300, 9400, 9500, 9600, 9700, 9800, 9900, 10000, 10100, 10200, 10300, 10400, 10500, 10600, 10700, 10800, 10900, 11000, 11100, 11200, 11300, 11400, 11500, 11600, 11700, 11800, 11900, 12000, 12250, 12500, 12750, 13000, 13250, 13500, 13750, 14000, 14250, 14500, 14750, 15000, 15250, 15500, 15750, 16000, 16250, 16500, 16750, 17000, 17250, 17500, 17750, 18000, 18250, 18500, 18750, 19000, 19250, 19500, 19750, 20000}; int hlen = 318;
   int step = 1000000;
   double *pp=0;double *hh=0; int ch=0;
   int u = interpolate(&p, &h,&t, &d, &a, &v, size,pp, hh, 0, ch);
