466ba53a7e25e08785c47c0d6bccce8c14a1ba17
Mat <tornado@amu.edu.pl>
2021-02-27 19:12:37 -0600

    Update sounding_wien.R

--- R/sounding_wien.R
+++ R/sounding_wien.R
@@ -1,29 +1,23 @@
 #' @name sounding_wien
 #' @aliases sounding_wien
-#' @title Examplary sounding dataset - sample from Wien (Austria) - 2011/08/23, 1200 UTC
+#' @title Examplary sounding dataset - sample from Vienna (WMO ID: 11035) - 23 August 2011, 1200 UTC
 #'
-#' @description The object contains pre-downloaded sounding dataset as obtained for Vienna rawinsonde station
-#' from http://weather.uwyo.edu/upperair/ with the use of the R climate package.
-#' The snapshot was taken on 2021/02/27 with the following syntax: 
+#' @description The object contains pre-downloaded sounding dataset from University of Wyoming sounding database.
+#' Dataset can be downloaded with the following syntax: 
 #' demo_dataset = get_sounding(wmo_id = 11035, yy = 2011, mm = 8, dd = 23, hh = 12)        
 #'
 #' @importFrom utils data
 #' @importFrom climate sounding_wyoming
 #' 
-#' @format A data frame with 88 rows and 11 variables as described in `get_sounding()`
+#' @format A data frame with 88 rows and 6 variables as described in `get_sounding()`
 #'
 #' \describe{
-#'   \item{pressure}{Air pressure in hPa}
-#'   \item{altitude}{Height in metres}
-#'   \item{temp}{Air temperutre in degree Celsius}
-#'   \item{dpt}{Dew point temperutre in degree Celsius}
-#'   \item{rh}{Relative humidity in percent}
-#'   \item{mixr}{Mixing ratio in g/kg}
-#'   \item{wd}{Wind direction in degrees}
-#'   \item{ws}{Wind speed in knots}
-#'   \item{thta}{K}
-#'   \item{thte}{K}
-#'   \item{thtv}{K}
+#'   \item{pressure}{pressure [hPa]}
+#'   \item{altitude}{altitude [m]}
+#'   \item{temp}{temperutre [degree Celsius]}
+#'   \item{dpt}{dew point temperutre [degree Celsius]}
+#'   \item{wd}{wind direction [azimuth as degrees]}
+#'   \item{ws}{wind speed [kn]}
 #' }
 #' 
 #' @source http://weather.uwyo.edu/upperair/sounding.html
