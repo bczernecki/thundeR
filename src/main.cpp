73aaafbee12ca4bca7d09b8df8c6fe51632ced1a
retsuz <67237477+retsuz@users.noreply.github.com>
2021-08-16 20:05:15 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 const double kel = 273.15;
 const double g = 9.81;
+
 class IndicesCollector;
 class InfoCollector;
 class Sounding;
@@ -3042,16 +3043,16 @@ void listToArray(list<double> list, double * arr, int len){
 	}
 }
 
-int interpolate(double **pu, double **hu, double **tu, double **du, double **au, double **vu, int n, double *p_arr=0, double *h_arr=0, int m=0, int o=0){
+int interpolate(double *&pu, double *&hu, double *&tu, double *&du, double *&au, double *&vu, int n, double *p_arr=0, double *h_arr=0, int m=0, int o=0){
     double defp[]={850,700,500}; int plen = 3;
     double defh[]={0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000, 3200, 3400, 3600, 3800, 4000, 4200, 4400, 4600, 4800, 5000, 5200, 5400, 5600, 5800, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000, 11500, 12000, 12500, 13000, 13500, 14000, 15000, 16000, 17000, 18000, 19000, 20000}; int hlen = 60;
     
-    double * p = *pu;
-    double * h = *hu;
-    double * t = *tu;
-    double * d = *du;
-    double * a = *au;
-    double * v = *vu;
+    double * p = pu;
+    double * h = hu;
+    double * t = tu;
+    double * d = du;
+    double * a = au;
+    double * v = vu;
     if(p_arr==0) p_arr=defp;
     else plen=m;
     if(h_arr==0)h_arr=defh;
@@ -3147,24 +3148,24 @@ int interpolate(double **pu, double **hu, double **tu, double **du, double **au,
         listToArray(va, na, u);
         listToArray(vv, nv, u);
        
-        delete[](*pu);delete[](*hu);delete[](*tu);delete[](*du);delete[](*au);delete[](*vu);
+        delete[](pu);delete[](hu);delete[](tu);delete[](du);delete[](au);delete[](vu);
        
-        *pu=np;*hu=nh;*tu=nt;*du=nd;*au=na;*vu=nv;
+        pu=np;hu=nh;tu=nt;du=nd;au=na;vu=nv;
         np=0;nh=0;nt=0;nd=0;na=0;nv=0;
         return u;
        
    
 }
-int interpolate2(double **pu, double **hu, double **tu, double **du, double **au, double **vu, int n, double *p_arr=0, double *h_arr=0, int m=0, int o=0){
+int interpolate2(double *&pu, double *&hu, double *&tu, double *&du, double *&au, double *&vu, int n, double *p_arr=0, double *h_arr=0, int m=0, int o=0){
     double defp[]={850,700,500}; int plen = 3;
     double defh[]={0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000, 3200, 3400, 3600, 3800, 4000, 4200, 4400, 4600, 4800, 5000, 5200, 5400, 5600, 5800, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000, 11500, 12000, 12500, 13000, 13500, 14000, 15000, 16000, 17000, 18000, 19000, 20000}; int hlen = 60;
   
-    double * p = *pu;
-    double * h = *hu;
-    double * t = *tu;
-    double * d = *du;
-    double * a = *au;
-    double * v = *vu;
+    double * p = pu;
+    double * h = hu;
+    double * t = tu;
+    double * d = du;
+    double * a = au;
+    double * v = vu;
     if(p_arr==0) p_arr=defp;
     else plen=m;
     if(h_arr==0)h_arr=defh;
@@ -3261,9 +3262,9 @@ int interpolate2(double **pu, double **hu, double **tu, double **du, double **au
         listToArray(va, na, u);
         listToArray(vv, nv, u);
        
-        delete[](*pu);delete[](*hu);delete[](*tu);delete[](*du);delete[](*au);delete[](*vu);
+        delete[](pu);delete[](hu);delete[](tu);delete[](du);delete[](au);delete[](vu);
        
-        *pu=np;*hu=nh;*tu=nt;*du=nd;*au=na;*vu=nv;
+        pu=np;hu=nh;tu=nt;du=nd;au=na;vu=nv;
 		np=0;nh=0;nt=0;nd=0;na=0;nv=0;
         return u;
        
@@ -3296,7 +3297,7 @@ double * sounding_default2(double* pressure,
   double defh2[]={0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680, 690, 700, 710, 720, 730, 740, 750, 775, 800, 825, 850, 875, 900, 925, 950, 975, 1000, 1025, 1050, 1075, 1100, 1125, 1150, 1175, 1200, 1225, 1250, 1275, 1300, 1325, 1350, 1375, 1400, 1425, 1450, 1475, 1500, 1525, 1550, 1575, 1600, 1625, 1650, 1675, 1700, 1725, 1750, 1775, 1800, 1825, 1850, 1875, 1900, 1925, 1950, 1975, 2000, 2025, 2050, 2075, 2100, 2125, 2150, 2175, 2200, 2225, 2250, 2275, 2300, 2325, 2350, 2375, 2400, 2425, 2450, 2475, 2500, 2525, 2550, 2575, 2600, 2625, 2650, 2675, 2700, 2725, 2750, 2775, 2800, 2825, 2850, 2875, 2900, 2925, 2950, 2975, 3000, 3050, 3100, 3150, 3200, 3250, 3300, 3350, 3400, 3450, 3500, 3550, 3600, 3650, 3700, 3750, 3800, 3850, 3900, 3950, 4000, 4050, 4100, 4150, 4200, 4250, 4300, 4350, 4400, 4450, 4500, 4550, 4600, 4650, 4700, 4750, 4800, 4850, 4900, 4950, 5000, 5050, 5100, 5150, 5200, 5250, 5300, 5350, 5400, 5450, 5500, 5550, 5600, 5650, 5700, 5750, 5800, 5850, 5900, 5950, 6000, 6100, 6200, 6300, 6400, 6500, 6600, 6700, 6800, 6900, 7000, 7100, 7200, 7300, 7400, 7500, 7600, 7700, 7800, 7900, 8000, 8100, 8200, 8300, 8400, 8500, 8600, 8700, 8800, 8900, 9000, 9100, 9200, 9300, 9400, 9500, 9600, 9700, 9800, 9900, 10000, 10100, 10200, 10300, 10400, 10500, 10600, 10700, 10800, 10900, 11000, 11100, 11200, 11300, 11400, 11500, 11600, 11700, 11800, 11900, 12000, 12250, 12500, 12750, 13000, 13250, 13500, 13750, 14000, 14250, 14500, 14750, 15000, 15250, 15500, 15750, 16000, 16250, 16500, 16750, 17000, 17250, 17500, 17750, 18000, 18250, 18500, 18750, 19000, 19250, 19500, 19750, 20000}; int hlen = 318;
   int step = 1000000;
   double *pp=0;double *hh=0; int ch=0;
-  int u = interpolate(&p, &h,&t, &d, &a, &v, size,pp, hh, 0, ch);
+  int u = interpolate(p, h,t, d, a, v, size,pp, hh, 0, ch);
   if(custom_vec==2){
 	  hh=defh2;
 	  ch=hlen;
@@ -3311,7 +3312,7 @@ double * sounding_default2(double* pressure,
 	  tmp=0;
 	  ch=4000;
   }
-  u = interpolate2(&p, &h,&t, &d, &a, &v, u,pp, hh, 0, ch);
+  u = interpolate2(p, h,t, d,a, v, u,pp, hh, 0, ch);
   if(custom_vec==3)delete[] hh;
   
   double *result = processSounding(p,h,t,d,a,v,u,step,sret);
@@ -3596,4 +3597,3 @@ Rcpp::NumericVector sounding_default(Rcpp::NumericVector pressure,
 	return out;
 	
 }
-//c
