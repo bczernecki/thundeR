06f3fd8cd32bc734b50ba04cbc0903b7627e17fb
Mateusz Taszarek <tornado@amu.edu.pl>
2022-11-24 22:28:05 +0100

    temporary chaotic

--- R/sounding_export.R
+++ R/sounding_export.R
@@ -46,43 +46,8 @@ sounding_export = function(pressure, altitude, temp, dpt, wd, ws, accuracy = 3, 
                                export_profile = 1, accuracy = accuracy, interpolate_step = interpolate_step,
                               meanlayer_bottom_top = meanlayer_bottom_top, storm_motion = storm_motion)
   
-  LP = length(sounding_default(pressure = pressure, altitude = altitude, 
-                               temp = temp, dpt = dpt, wd = wd, ws = ws, 
-                               export_profile = 0, accuracy = 1, interpolate_step = interpolate_step,
-                              meanlayer_bottom_top = meanlayer_bottom_top, storm_motion = storm_motion)) # no. of parameters 
-###
-pozMU = parametry[LP+1]
-MUs = parametry[LP+2]
-MUw = parametry[(LP+3):(LP+2+pozMU)]
-###
-pozALL = parametry[LP+pozMU+3]
-SBw = parametry[(LP+pozMU+4):(LP+pozMU+pozALL+3)]
-MLw = parametry[(LP+pozMU+pozALL+5):(LP+pozMU+(2*pozALL)+4)]
-Pw = parametry[(LP+pozMU+(2*pozALL)+6):(LP+pozMU+(3*pozALL)+5)]
-Hw <- parametry[(LP+pozMU+(3*pozALL)+7):(LP+pozMU+(4*pozALL)+6)]
-Tw <- parametry[(LP+pozMU+(4*pozALL)+8):(LP+pozMU+(5*pozALL)+7)]
-TDw <- parametry[(LP+pozMU+(5*pozALL)+9):(LP+pozMU+(6*pozALL)+8)]
-WDw <- parametry[(LP+pozMU+(6*pozALL)+10):(LP+pozMU+(7*pozALL)+9)]
-WSw <- parametry[(LP+pozMU+(7*pozALL)+11):(LP+pozMU+(8*pozALL)+10)]
-DNw <- parametry[(LP+pozMU+(8*pozALL)+12):(LP+pozMU+(9*pozALL)+11)]
-TVw <- parametry[(LP+pozMU+(9*pozALL)+13):(LP+pozMU+(10*pozALL)+12)]
-MUw <- c(rep(NA,pozALL-pozMU),MUw) # correction for elevated MU
 
-###
-res <- data.frame(pressure = Pw,
-                  altitude = Hw,
-                  temp = Tw, 
-                  tempV = TVw, 
-                  dpt = TDw,
-                  wd = WDw,
-                  ws = WSw,
-                  MU = MUw,
-                  SB = SBw,
-                  ML = MLw,
-                  DN = DNw)
 
-res$DN[res$altitude-res$altitude[1] >= 4000] <- NA
-
-return(res)
+return(parametry)
   
 }
