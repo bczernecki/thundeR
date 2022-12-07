1af1f361024974f6d6e1f606dbe21c5be760753e
Mateusz Taszarek <tornado@amu.edu.pl>
2022-12-07 15:37:41 +0000

    Update sounding_compute.R

--- R/sounding_compute.R
+++ R/sounding_compute.R
@@ -235,10 +235,10 @@ sounding_compute = function(pressure, altitude, temp, dpt, wd, ws, accuracy = 2,
   export_profile = 0 
   
   if(sum(storm_motion==c(999,999)) != 2){
-  ws = storm_motion[1]
-  wd = storm_motion[2]
-  storm_motion[1] = round(-ws * 0.514444 * sin(wd * pi/180), 2)
-  storm_motion[2] = round(-ws * 0.514444 * cos(wd * pi/180), 2)
+  ws2 = storm_motion[1]
+  wd2 = storm_motion[2]
+  storm_motion[1] = round(ws2 * 0.514444 * sin(wd2 * pi/180), 2)
+  storm_motion[2] = round(-ws2 * 0.514444 * cos(wd2 * pi/180), 2)
   }  
   
   tmp = sounding_default(pressure, altitude, temp, dpt, wd, ws, export_profile, accuracy, interpolate_step, meanlayer_bottom_top, c(storm_motion[1],storm_motion[2],999))
