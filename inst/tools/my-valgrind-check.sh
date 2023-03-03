37fd75e67f89124b5ac5f4a4afd2ddeb89ce0c04
bczernecki <bczernecki@gmail.com>
2023-03-03 13:03:46 +0100

    my-valgrind

--- inst/tools/my-valgrind-check.sh
+++ inst/tools/my-valgrind-check.sh
@@ -11,10 +11,10 @@ mkdir -p valgrind-check
 # valgrind-check directory (uploaded as artifact in GH actions) some large
 # directories and files produced as part of R CMD check
 docker run --rm -v $(pwd):/thunder bczernecki/thunder3 bash -c ' \
-  R -e "install.packages(\"remotes\")" \
-  R -e "remotes::install_github(\"bczernecki/climate\", dependencies = TRUE)" \
   ls -alh \
   pwd \
+  R -e "install.packages(\"remotes\")" \
+  R -e "remotes::install_github(\"bczernecki/climate\", dependencies = TRUE)" \
   R -e "remotes::install_deps(\"thunder\", dependencies = TRUE)" \
   && R CMD build /thunder \
   && R CMD INSTALL thunder*.tar.gz \
