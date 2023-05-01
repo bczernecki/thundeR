9a42f0a923eb344ac40428c5e119a3f104806a1e
Mateusz Taszarek <tornado@amu.edu.pl>
2023-05-02 00:24:26 +0200

    Update sounding_compute.R

--- R/sounding_compute.R
+++ R/sounding_compute.R
@@ -225,9 +225,9 @@
 #' @param accuracy accuracy of computations where 3 = high (slow), 2 = medium (recommended), 1 = low (fast)
 #' @param interpolate_step interpolation step to be used for vertical interpolation. Valid only if `accuracy` is set to 3 (default is 5 m)
 #' @param meanlayer_bottom_top (optional) vector of length 2 for bottom and top heights used for computing parcel starting parameters; default: 0, 500
-#' @param ECAPE_inflow (optional) user-defined storm inflow for entrainment CAPE (ECAPE) calculation, 01km_RM as default.
 #' @param storm_motion (optional) for moving storms only - one can define vector of length two with
 #' wind speed (m/s) and wind directions (degrees) that will be used to compute adjusted SRH parameters
+#' @param ECAPE_inflow (optional) user-defined storm inflow for entrainment CAPE (ECAPE) calculation, 01km_RM as default.
 #' @return Named vector of 200+ convective indices
 #' @export 
 #' @examples
