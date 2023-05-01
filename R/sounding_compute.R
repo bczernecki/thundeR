e5eb67788042b6563f35b31ebf866824a4868486
Mateusz Taszarek <tornado@amu.edu.pl>
2023-05-02 00:06:08 +0200

    Update sounding_compute.R

--- R/sounding_compute.R
+++ R/sounding_compute.R
@@ -247,7 +247,7 @@ sounding_compute = function(pressure, altitude, temp, dpt, wd, ws,
                             interpolate_step = 5,
                             meanlayer_bottom_top = c(0, 500),
                             storm_motion = c(999, 999),
-                            ECAPE_inflow = "01km_RM") {
+                            ECAPE_inflow) {
   
   export_profile = 0 
   
