55b7c978ded29b431e4a0c9c79d9b907dcab6371
Mateusz Taszarek <tornado@amu.edu.pl>
2023-06-27 14:20:13 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -885,7 +885,7 @@ void Kinematics::doSRH(int i, double p, double h, double t, double d, double a,d
     double h_MU = this->muheight;
     Vector v_ = Get(this->vw, i);
     
-    if (h-h_MU >= 0 && h-h_MU >= 1000)
+    if (h-h_MU >= 0 && h-h_MU <= 1000)
     {
       mean01eff += v_;
       n1eff += 1;
