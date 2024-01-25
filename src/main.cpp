bbf006dc9b7bf375fe32ddd6373ffdc488c801d9
Mateusz Taszarek <tornado@amu.edu.pl>
2024-01-25 12:54:11 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2220,6 +2220,7 @@ void Thermodynamics::putSpecificLine(int i, double p, double h, double t, double
   mostUnstable->putLine(i, p, h, t, d, a, v);
   mostU500->putLine(i, p, h, t, d, a, v);
   meanmostUnstable->putLine(i, p, h, t, d, a, v);
+  meanLayer->putLine(i, p, h, t, d, a, v);
   lastp = p;
   lastt = t;
   lasth = h;
@@ -2248,7 +2249,6 @@ void Thermodynamics::prepareMeanLayer()
 }
 void Thermodynamics::putMeanLine(int i, double p, double h, double t, double d, double a, double v)
 {
-  this->meanLayer->putLine(i, p, h, t, d, a, v);
   determineDowndraftByMinTHTE(i, p, h, t, d, a, v);
   putShowalter(i, p, h, t, d, a, v);
 }
