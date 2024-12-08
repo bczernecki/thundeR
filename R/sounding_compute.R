c7a0cfedd02fce24efd2ffa02d807e6a8bddf820
Mateusz Taszarek <tornado@amu.edu.pl>
2024-12-08 21:06:26 +0100

    Update sounding_compute.R

--- R/sounding_compute.R
+++ R/sounding_compute.R
@@ -157,6 +157,7 @@
 #'  \item 	HGT_ISO_0
 #'  \item 	HGT_ISO_0_wetbulb
 #'  \item 	HGT_ISO_M10
+#'  \item 	HGT_ISO_M10_wetbulb  
 #'  \item 	HGT_MU
 #'  \item 	HGT_MUML
 #'  \item 	THETAE_delta
@@ -178,6 +179,7 @@
 #'  \item 	RH_500850
 #'  \item 	RH_MU_LCL_3km
 #'  \item 	RH_MUML_LCL_3km
+#'  \item 	RH_MU5_LCL_3km
 #'  \item 	PRCP_WATER
 #'  \item 	PRCP_WATER_eff
 #'  \item 	Moisture_Flux_0500m
@@ -321,17 +323,6 @@
 #'  \item 	SHERB_mod
 #'  \item 	DEI
 #'  \item 	DEI_eff
-#'  \item 	MU5_E_CAPE_M10
-#'  \item 	RH_ML_LCL_3km
-#'  \item 	RH_ML_LCL_1500m
-#'  \item 	RH_MU5_LCL_3km
-#'  \item 	RH_MU5_LCL_1500m
-#'  \item 	RH_MU_LCL_1500m
-#'  \item 	RH_MUML_LCL_1500m
-#'  \item 	HGT_ISO_M10_wetbulb  
-#'  \item 	MU_LI_M15
-#'  \item 	MUML_LI_M15
-#'  \item 	MU5_LI_M15
 #' }
 #'
 #' @param pressure pressure [hPa]
@@ -535,6 +526,7 @@ sounding_compute = function(pressure, altitude, temp, dpt, wd, ws,
 "HGT_ISO_0",
 "HGT_ISO_0_wetbulb",
 "HGT_ISO_M10",
+"HGT_ISO_M10_wetbulb",
 "HGT_MU",
 "HGT_MUML",
 "THETAE_delta",
@@ -556,6 +548,7 @@ sounding_compute = function(pressure, altitude, temp, dpt, wd, ws,
 "RH_500850",
 "RH_MU_LCL_3km",
 "RH_MUML_LCL_3km",
+"RH_MU5_LCL_3km",
 "PRCP_WATER",
 "PRCP_WATER_eff",
 "Moisture_Flux_0500m",
@@ -698,17 +691,6 @@ sounding_compute = function(pressure, altitude, temp, dpt, wd, ws,
 "SHERBE",
 "SHERB_mod",
 "DEI",
-"DEI_eff",
-"MU5_E_CAPE_M10",
-"RH_ML_LCL_3km",
-"RH_ML_LCL_1500m",
-"RH_MU5_LCL_3km",
-"RH_MU5_LCL_1500m",
-"RH_MU_LCL_1500m",
-"RH_MUML_LCL_1500m",
-"HGT_ISO_M10_wetbulb",
-"MU_LI_M15",
-"MUML_LI_M15",
-"MU5_LI_M15")
+"DEI_eff")
 return(tmp)
 }
