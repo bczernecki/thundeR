0f918b19d99fb91664cbc6f06d4ddb5b3a924fa8
Mateusz Taszarek <tornado@amu.edu.pl>
2025-09-06 21:52:45 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7605,10 +7605,6 @@ double * processSounding(double *p_, double *h_, double *t_, double *d_, double 
   vec[328]=(*S)->getIndicesCollectorPointer()->M30Height();
   vec[329]=(*S)->getIndicesCollectorPointer()->MU5_cold_cloud(); 
   vec[330]=(*S)->getIndicesCollectorPointer()->MU5_equal_layer(); 
-  vec[331]=(*S)->getIndicesCollectorPointer()->Ventilation_25km_RM();
-  vec[332]=(*S)->getIndicesCollectorPointer()->Ventilation_25km_LM();
-  vec[333]=(*S)->getIndicesCollectorPointer()->Ventilation_25km();
-  vec[334]=(*S)->getIndicesCollectorPointer()->Ventilation_16km();
   return vec;
 }
 
@@ -8319,7 +8315,7 @@ double * sounding_default2(double* pressure,
    int mulen,sblen,mllen,dnlen,mustart,mlstart;
    
    double *result = sounding_default2(p,h,t,d,a,v,size,&sret,q, interpolate_step, mlp, sm);
-   int reslen= 335;
+   int reslen= 331;
    int maxl=reslen;
    if(export_profile[0]==1){
      plen = sret->p->size();
